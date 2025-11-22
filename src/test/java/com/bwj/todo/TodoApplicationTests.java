a87dbf25cf17f7640cd532be55c4da33090ada4e
WonJin <try3982@kakao.com>
2025-11-22 18:50:43 +0900

    -. 게시글 crud

---
+++ src/test/java/com/bwj/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bwj.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
